24d814f6b29172acc4ff1190d6fa0d31e9d8f185
Swoosky <agushusky@gmail.com>
2021-12-04 20:53:55 +0800

    fix routes wtf is going on help me

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\TicketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,9 +19,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('ticket', 'app\Http\Controllers\Api\TicketController@index');
-Route::get('ticket/{user}', 'app\Http\Controllers\Api\TicketController@indexOneUser');   
-Route::get('ticket/{id}', 'app\Http\Controllers\Api\TicketController@show');
-Route::post('ticket', 'app\Http\Controllers\Api\TicketController@store');
-Route::put('ticket/{id}', 'app\Http\Controllers\Api\TicketController@update');
-Route::delete('ticket/{id}', 'app\Http\Controllers\Api\TicketController@destroy');
+Route::get('ticket', [TicketController::class, 'index']);
+Route::get('ticket/{user}', [TicketController::class, 'indexOneUser']);   
+Route::get('ticket/{id}', [TicketController::class, 'show']);
+Route::post('ticket', [TicketController::class, 'store']);
+Route::put('ticket/{id}', [TicketController::class, 'update']);
+Route::delete('ticket/{id}', [TicketController::class, 'destroy']);
