668973782c0ee0fc92207aedd69c708d350cf8ea
Swoosky <agushusky@gmail.com>
2021-12-04 20:49:40 +0800

    test ticket2controller

--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('ticket', 'Api\TicketController@index');
-Route::get('ticket/{user}', 'Api\TicketController@indexOneUser');   
-Route::get('ticket/{id}', 'Api\TicketController@show');
-Route::post('ticket', 'Api\TicketController@store');
-Route::put('ticket/{id}', 'Api\TicketController@update');
-Route::delete('ticket/{id}', 'Api\TicketController@destroy');
+Route::get('ticket', 'Api\Ticket2Controller@index');
+Route::get('ticket/{user}', 'Api\Ticket2Controller@indexOneUser');   
+Route::get('ticket/{id}', 'Api\Ticket2Controller@show');
+Route::post('ticket', 'Api\Ticket2Controller@store');
+Route::put('ticket/{id}', 'Api\Ticket2Controller@update');
+Route::delete('ticket/{id}', 'Api\Ticket2Controller@destroy');
